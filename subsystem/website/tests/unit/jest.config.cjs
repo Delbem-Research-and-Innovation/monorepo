2b0024f49f65d193e1f1dc1704eba429ce67f78a
naneshoru <ricardo_cssg@hotmail.com>
2026-05-14 17:07:01 -0300

    fix(tests): add jest mock for next/cache and update jest config

--- subsystem/website/tests/unit/jest.config.cjs
+++ subsystem/website/tests/unit/jest.config.cjs
@@ -2,4 +2,10 @@ const { jestUnitConfig } = require('@ttoss/config');
 
 module.exports = jestUnitConfig({
   setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+  moduleNameMapper: {
+    '^next/cache$': '<rootDir>/mocks/next-cache.cjs',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@ttoss/|\\.pnpm/)',
+  ],
 });
